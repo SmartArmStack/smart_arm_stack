5489e6d50c9959aed9e3a2a0e3fc95d30499eeba
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2022-02-14 12:55:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd ~/smart_arm_stack_installation
 sudo apt remove ros-noetic-sas* -y
 # Download files
 wget $(curl -sL https://api.github.com/repos/smartarmstack/smart_arm_stack_lgpl/releases/latest | jq -r '.assets[].browser_download_url')
-wget $(curl -sL https://api.github.com/repos/smartarmstack/smart_arm_stack_research_only/releases/latest | jq -r '.assets[].browser_download_url')
+wget $(curl -sL https://api.github.com/repos/smartarmstack/smart_arm_stack_researchonly/releases/latest | jq -r '.assets[].browser_download_url')
 # Install packages
 sudo dpkg -i  ros-noetic-sas-*.deb
 # Remove temporary folder
